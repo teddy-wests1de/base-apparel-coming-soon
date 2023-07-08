4a9f6c7cc37bb288719540a9ec9ef10f3b501abe
Elrico Landrew <dirkieys21@gmail.com>
2023-07-08 16:01:55 +0200

    Fixed Validation Fields

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const submitBtn = document.querySelector(".submit");
 const validateEmail = document.querySelector(".validation");
 const error = document.querySelector('.error-icon');
 
-// console.log(emailInput.value);
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
   if (validEmail(emailInput.value)) {
     validateEmail.classList.add('hidden')
     error.classList.add('hidden')
-    console.log(emailInput.value);
     emailInput.style.borderColor = "hsl(0, 88%, 77%)";
   } else {
     validateEmail.innerHTML = "Please provide a valid email";
